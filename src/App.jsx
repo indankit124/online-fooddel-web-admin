41652477be4faf44bab0cc28f2ba627a38f029b5
indankit124 <152060353+indankit124@users.noreply.github.com>
2024-08-28 13:45:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = 'http://localhost:4000'
+  const url = 'https://backend-fooddev.onrender.com'
   return (
     <div>
       <ToastContainer />
